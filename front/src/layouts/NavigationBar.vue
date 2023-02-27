757117dbe01b376da6da95a443406e2da31ddd16
Jf08ml <lassojuanfe@gmail.com>
2023-02-27 17:58:33 -0500

    commit userlogoutmobil

--- front/src/layouts/NavigationBar.vue
+++ front/src/layouts/NavigationBar.vue
@@ -66,32 +66,45 @@
     </div>
 
     <div class="menu-hamburger">
+      <div class="nav-link" v-if="authStore.token">
+          <button @click="logOut()" class="nav-button-menu-logout"><v-icon name="co-account-logout" scale="1.3"
+              title="Real Comics" /></button>
+          <router-link custom to="/perfil">
+            <template v-slot="{ navigate }">
+              <button @click="navigate" @mouseover="showMenuUser = true" class="nav-button-icone"><v-icon
+                  name="ri-account-pin-circle-fill" scale="1.5" title="Real Comics" color="white" /></button>
+            </template>
+          </router-link>
+        </div>
       <div class="hamburger-icon" @click="toggleMenu">
           <v-icon v-if="!isOpen" name="ri-menu-fill" scale="2" title="Menu" color="white" />
           <v-icon v-if="isOpen" name="md-close-sharp" scale="2" title="Menu" color="red" />
       </div>
+      
       <div class="menu" :class="{ 'menu-open': isOpen }">
         <router-link custom to="/realcomics">
           <template v-slot="{ navigate }">
-            <button @click="navigate" style="width: 40%; font-size: 20px; color: #b81f59;"><v-icon name="fa-person-booth"
+            <div style="width: 100%; font-size: 20px; color: #b81f59;">
+            <button @click="() => {navigate(); isOpen = false;}" style="width: 100%; font-size: 20px; color: #b81f59;"><v-icon name="fa-person-booth"
                 scale="1.2" title="Real Comics" color="#b81f59" /> Real</button>
+              </div>
           </template>
         </router-link>
         <router-link custom to="/animatedcomics">
           <template v-slot="{ navigate }">
-            <button @click="navigate" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon name="si-gitbook"
+            <button @click="() => {navigate(); isOpen = false;}" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon name="si-gitbook"
                 scale="1.5" title="Real Comics" color="#b81f59" /> Animated</button>
           </template>
         </router-link>
-        <router-link custom to="/login">
+        <router-link custom to="/login" v-if="!authStore.token">
           <template v-slot="{ navigate }">
-            <button @click="navigate" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon name="md-login"
+            <button @click="() => {navigate(); isOpen = false;}" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon name="md-login"
                 scale="1.2" title="Real Comics" color="#b81f59" /> Log In</button>
           </template>
         </router-link>
-        <router-link custom to="/signup">
+        <router-link custom to="/signup" v-if="!authStore.token">
           <template v-slot="{ navigate }">
-            <button @click="navigate" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon
+            <button @click="() => {navigate(); isOpen = false;}" style="width: 100%;font-size: 20px; color: #b81f59"><v-icon
                 name="md-newlabel-twotone" scale="1.2" title="Real Comics" color="#b81f59" /> Sign Up</button>
           </template>
         </router-link>
@@ -147,16 +160,19 @@ const logOut = () => {
   router.push("/login");
 }
 
+
 </script>
 
 <style lang="scss" scoped>
 .menu-hamburger {
   position: relative;
+  display: flex;
+  justify-content: center;
 
   .hamburger-icon {
 
     cursor: pointer;
-    margin-right: 10px;
+    margin-inline: 10px;
 
     @media screen and (min-width: 900px) {
       display: none;
